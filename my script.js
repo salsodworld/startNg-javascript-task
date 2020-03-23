423affb941ed69f78ea97ec9f52a5586c363d17a
Sodiq <salsod1st@gmail.com>
2020-03-23 01:13:26 +0100

    Add files via upload

---
+++ my script.js
@@ -0,0 +1,10 @@
+var myName = "Salaudeen Sodiq";
+var courses = ["Front end development"];
+
+if(courses.length % 2 === 1) {
+  for(var i = 0, max = 20; i <= max; i++) {
+    if(i % 2 === 1) {
+      console.log(i);
+    }
+  }
+}
